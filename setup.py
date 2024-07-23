d9e96f8d22a7f12d50b19a1d355fce8d78845998
Nick Wogan <nicholaswogan@gmail.com>
2024-07-23 15:56:30 -0700

    install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+            
+setup(
+    name="hotjupchem",
+    packages=['hotjupchem'],
+    python_requires='>=3.6',
+    version="0.1.0", 
+    author='Nicholas Wogan',
+    author_email = 'nicholaswogan@gmail.com',
+    include_package_data=True
+)
