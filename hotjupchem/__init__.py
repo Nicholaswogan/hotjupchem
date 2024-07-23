ccd48e0cee2a31427743bf4cfa39432772a07a7c
Nick Wogan <nicholaswogan@gmail.com>
2024-07-23 15:55:24 -0700

    paths to default files

--- hotjupchem/__init__.py
+++ hotjupchem/__init__.py
@@ -1,3 +1,5 @@
 from driver import EvoAtmosphereHJ, DATA_DIR
 from climate import ClimateHJ
 
+zahnle_earth_HHeCNOS = DATA_DIR+'zahnle_earth_HHeCNOS.yaml'
+zahnle_earth_thermo = DATA_DIR+'zahnle_earth_thermo.yaml'
