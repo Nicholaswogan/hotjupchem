5891cd561314a0e96a8c6a72566a59e998735374
Nick Wogan <nicholaswogan@gmail.com>
2024-07-23 17:22:29 -0700

    routine for dealing with stellar flux

--- hotjupchem/utils.py
+++ hotjupchem/utils.py
@@ -4,6 +4,8 @@ from numba import types
 from scipy import constants as const
 from scipy import integrate
 from photochem import equilibrate
+from picaso import fluxes
+from matplotlib import pyplot as plt
 
 class Metallicity():
 
@@ -240,3 +242,99 @@ def compute_altitude_of_PT(P, P_ref, T, mubar, planet_radius, planet_mass, P_top
     z_ = np.append(out1.y[0][::-1],out2.y[0])
 
     return P_, T_, mubar_, z_
+
+def equilibrium_temperature(stellar_radiation, bond_albedo):
+    T_eq = ((stellar_radiation*(1.0 - bond_albedo))/(4.0*const.sigma))**(0.25)
+    return T_eq 
+
+def bolometric_flux(Teq, bond_albedo):
+    stellar_radiation = 4.0*const.sigma*Teq**4/(1.0 - bond_albedo)
+    return stellar_radiation
+
+def _bolometric_flux(wv, F):
+    return 1e-3*np.sum(F[:-1]*(wv[1:]-wv[:-1]))
+
+def write_photochem_spectrum(wv, F, bolometric_radiation, Teff, outfile, plot=True):
+    """Generates a UV flux file compatable with Photochem
+
+    Parameters
+    ----------
+    wv : ndarray
+        Array of wavelengths in nm
+    F : ndarray
+        Array of stellar fluxes in mW/m^2/nm
+    bolometric_radiation : float
+        The total irradiation at a planet in W/m^2
+    Teff : float
+        The stellar effective temperature
+    outfile : str
+        Output file name
+    plot : bool, optional
+        If True, then a plot is made, by default True
+    """    
+    if wv[-1]/1e3 < 100.0:
+        # Add a blackbody spectrum to 100 microns
+        wvb_cm = np.linspace((wv[-1]+wv[-1]*1e-5)/1e3,100,1000)/1e4
+        wvb_nm = wvb_cm*(1/1e2)*(1e9/1) 
+        Fb = fluxes.blackbody(Teff, wvb_cm)[0]*(1e2/1)*(1/1e9) # convert to mW/m^2/nm
+
+        # Scale the blackbody, so it fits with the original spectrum
+        factor = F[-1]/Fb[0]
+        Fb = Fb*factor
+
+        # Stitch together spectrum and blackbody
+        wv_new = np.append(wv,wvb_nm)
+        F_new = np.append(F,Fb)
+    else:
+        wv_new = wv
+        F_new = F
+
+    # Scale to match solar constant
+    stellar_constant_tmp = _bolometric_flux(wv_new, F_new)
+    factor = bolometric_radiation/stellar_constant_tmp
+    F_new = F_new*factor
+
+    # Write the output file
+    fmt = '{:30}'
+    with open(outfile,'w') as f:
+        f.write(fmt.format('Wavelength (nm)'))
+        f.write(fmt.format('Solar flux (mW/m^2/nm)'))
+        f.write('\n')
+        for i in range(F_new.shape[0]):
+            f.write(fmt.format('%e'%wv_new[i]))
+            f.write(fmt.format('%e'%F_new[i]))
+            f.write('\n')
+
+    if plot:
+        # Plot
+        plt.rcParams.update({'font.size': 15})
+        fig,ax = plt.subplots(1,1,figsize=[6,5])
+        fig.patch.set_facecolor("w")
+
+        # Plot the spectrum
+        ax.plot(wv_new,F_new,lw=.5, c='k', label='Spectrum')
+
+        # Plot blackbody spectrum
+        wvb_cm = np.linspace(.1,100,10000)/1e4
+        wvb_nm = wvb_cm*(1/1e2)*(1e9/1) 
+        Fb = fluxes.blackbody(Teff, wvb_cm)[0]*(1e2/1)*(1/1e9) # convert to mW/m^2/nm
+        stellar_constant_b = _bolometric_flux(wvb_nm, Fb)
+        factor = bolometric_radiation/stellar_constant_b
+        Fb = Fb*factor
+        ax.plot(wvb_nm,Fb,alpha=0.4,label='T = %i K blackbody'%(Teff))
+
+        stellar_constant = _bolometric_flux(wv_new, F_new)
+        T_eq = equilibrium_temperature(stellar_constant, 0.0)
+        note = 'Bolometric flux = %.1f W/m$^2$\n'%(stellar_constant) \
+            +'Equilibrium Temperature = %i K'%(T_eq)
+        ax.text(.97, .02, note, \
+                size = 11, ha='right', va='bottom',transform=ax.transAxes)
+        ax.set_yscale('log')
+        ax.set_xscale('log')
+        ax.set_xlim(1e1,1e5)
+        ax.set_ylim(1e-5,2e3)
+        ax.set_xlabel('Wavelength (nm)')
+        ax.set_ylabel('Stellar flux (mW m$^{-2}$ nm$^{-1}$)')
+        ax.grid(alpha=0.8)
+        ax.legend(ncol=1,bbox_to_anchor=(0.02,1.05),loc='lower left',fontsize=13)
+        plt.show()
